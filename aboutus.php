7dd93204e4387976af0e13956f41a0be0a3a2d8f
Jason <Jason@hoffmantrucking.com>
2018-05-12 09:10:16 -0700

    menu

--- aboutus.php
+++ aboutus.php
@@ -14,8 +14,8 @@
       <section id="about-intro" class="clear">
         <h2>About Us</h2>
         <img src="images/960x340.gif" alt="">
-        <p>In sed neque id libero pretium luctus. Vivamus faucibus. Ut vitae elit. In hac habitasse platea dictumst. Proin et nisl ac orci tempus luctus. Aenean lacinia justo at nisi. Vestibulum sed eros sit amet nisl lobortis commodo. Suspendisse nulla. Vivamus ac lorem. Aliquam pulvinar purus at felis. Quisque convallis nulla id ipsum. Praesent vitae urna. Fusce blandit nunc nec mi. Praesent vestibulum hendrerit ante.</p>
-        <p>Vivamus accumsan. Donec molestie pede vel urna. Curabitur eget sem ornare felis gravida vestibulum.Sed pulvinar, tellus in venenatis vehicula, lorem magna dignissim erat, in accumsan ante lorem sit amet lorem.</p>
+        <p>Hoffman Trucking provides a large variety of materials for completion of your project. We offer a wide variety of landscaping materials for all of your landscaping projects big or small. With excellent customer service and the best pricing in the region for decorative rock, flagstone, sand, and organic materials we are sure we can help you find what you need for a price that you’ll love.</p>
+        <p>We can Deliver materials right to your door.</p>
       </section>
       <!-- ####################################################################################################### -->
       <section id="team">
@@ -26,7 +26,7 @@
               <figcaption>
                 <p class="team_name">Ed Hoffman</p>
                 <p class="team_title">Owner/Operator</p>
-                <p class="team_description">Founded in 2005, Hoffman trucking has provides a large variety of materials for completion of your project. We offer a wide variety of landscaping materials for all of your landscaping projects big or small. With excellent customer service and the best pricing in the region for decorative rock, flagstone, sand, and organic materials we are sure we can help you find what you need for a price that you’ll love.</p>
+                <p class="team_description">Founded in 2005, Hoffman trucking </p>
               </figcaption>
             </figure>
           </li>
@@ -36,42 +36,6 @@
                 <p class="team_name">Jim Smith</p>
                 <p class="team_title">Operator</p>
                 <p class="team_description">Jim has been operating heavy mechinery for over a decade and will provide excelent care of you projects site when delivoring your requested materials.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="one_third">
-            <figure><img src="images/team-member.gif" alt="">
-              <figcaption>
-                <p class="team_name">Name Goes Here</p>
-                <p class="team_title">Job Title Here</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="one_third first">
-            <figure><img src="images/team-member.gif" alt="">
-              <figcaption>
-                <p class="team_name">Name Goes Here</p>
-                <p class="team_title">Job Title Here</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="one_third">
-            <figure><img src="images/team-member.gif" alt="">
-              <figcaption>
-                <p class="team_name">Name Goes Here</p>
-                <p class="team_title">Job Title Here</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="one_third">
-            <figure><img src="images/team-member.gif" alt="">
-              <figcaption>
-                <p class="team_name">Name Goes Here</p>
-                <p class="team_title">Job Title Here</p>
-                <p class="team_description">Vestassapede et donec ut est liberos sus et eget sed eget. Quisqueta habitur augue magnisl magna phas ellus sagit titor ant curabi turpis.</p>
               </figcaption>
             </figure>
           </li>
