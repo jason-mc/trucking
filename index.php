492182add141854597a26214985ce79d7ebbb079
Jason <Jason@hoffmantrucking.com>
2018-05-12 08:44:24 -0700

    aboutus

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php echo file_get_contents("html/header1.html"); ?>
-<title>Hoffman Trucking</title>
-<?php echo file_get_contents("html/header2.html"); ?>
-This is page 1, the home page.
-<?php echo file_get_contents("html/footer.html"); ?>
+<?php
+header('Location:/trucking/aboutus.php');
+?>
